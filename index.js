8416d246cf3b571d70f8f4680d655d2a253381fe
Warren Zhang <warrenzhang17@gmail.com>
2023-03-20 20:01:29 -0400

    arithmetic complete

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random = Math.floor(Math.random() * 10);
+console.log(random);
+
+let num3 = 28;
+let num4 = 7;
+let mod = num3 / num4;
+console.log(mod);
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+console.log(max);
